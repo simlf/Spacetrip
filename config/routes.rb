c9c3b2f6606afaa07f9984fea6440812a1d63ffa
aristide riou <aristide.riou@gmail.com>
2019-06-03 18:57:41 +0200

    started implementing the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :ships :only [:new, :create, :show] do
+    resources :bookings :only [:create, :new]
+  end
+
+  resources :bookings :only [:index]
+
+  namespace :owner do
+    resources :ships :only [:index, :new, :create]
+    resources :bookings :only [:index]
+  end
 end
