8bdada17d0f1c006a997ec44182b73e4162d9719
valcwt <cauwet.valentin@gmail.com>
2019-06-03 18:52:27 +0200

    valid models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :ship
-  has_many :users
-  has_many :ships
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,4 @@
 class Ship < ApplicationRecord
   belongs_to :user
-  has_many :users
+  has_many :bookings
 end
