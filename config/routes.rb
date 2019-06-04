61fb99edcefea550a621b552dfbb6724bebd5dcf
aristide riou <aristide.riou@gmail.com>
2019-06-04 10:38:13 +0200

    finished routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,24 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :ships :only [:new, :create, :show] do
-    resources :bookings :only [:create, :new]
+  resources :ships, only: [:show] do
+    resources :bookings, only: [:create, :new]
   end
 
-  resources :bookings :only [:index]
+  resources :bookings, only: [:index]
+
+  # Accept / refuse request bookings
+  # As an owner, I can see / accept / refuse all the pending bookings for my ships
+  # --[PATCH owner/bookings/booking_id/accept]
+  # -- [PATCH owner/bookings/booking_id/refuse]--
 
   namespace :owner do
-    resources :ships :only [:index, :new, :create]
-    resources :bookings :only [:index]
+    resources :ships, only: [:index, :new, :create]
+    resources :bookings, only: [:index] do
+      member do
+        patch 'accept'
+        patch 'refuse'
+      end
+    end
   end
 end
