e4f1f9c520b1702f4fcf0a749a4dd123b71b1ff8
simlf <lefournsimon@gmail.com>
2019-06-06 14:20:51 +0200

    fixing

--- app/controllers/owner/ships_controller.rb
+++ app/controllers/owner/ships_controller.rb
@@ -2,4 +2,18 @@ class Owner::ShipsController < ApplicationController
   def index
     @ships = current_user.ships
   end
+
+  def new
+    @ship = Ship.new
+  end
+
+  def create
+    @ship = ship.new(ship_params)
+
+    if @ship.save
+      redirect_to ship_path(@ship)
+    else
+      render :new
+    end
+  end
 end
