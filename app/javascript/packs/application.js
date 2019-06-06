7572b4c4e07d7b421c37f6565b6d821bdb26eabb
aristide riou <aristide.riou@gmail.com>
2019-06-06 16:59:01 +0200

    implemented the price dynamic feature on ship page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,16 @@
-import "bootstrap";
-import "../plugins/flatpickr"
+import 'bootstrap';
+import '../plugins/flatpickr';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
+// const setDynamicPrice => {
+//   const shipPrice = document.getElementById('rails-data').dataset.price;
+//   let msDiff = selectedDates[1].getTime()-selectedDates[0].getTime();
+//   let daysDiff = msDiff/8.64e+7;
+//   document.getElementById('dynamic_price').innerHTML = shipPrice * daysDiff;
+// }
 
+// export {setDynamicPrice}
