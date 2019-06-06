796ad2fe5a9e257d3c978046ceebe6b9f4009423
simlf <lefournsimon@gmail.com>
2019-06-06 11:55:39 +0200

    Date picker added to the ships view

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,8 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/themes/dark.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+
+flatpickr("#range_start", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
